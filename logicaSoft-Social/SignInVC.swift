88f2b9c7f5581d3d56c9ac21af3232a85f3823d5
Venkat Madira <VenkatMadira@venkat-madira.home>
2017-06-17 11:18:32 +0100

    Firebase integration to project

---
+++ logicaSoft-Social/SignInVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  logicaSoft-Social
+//
+//  Created by Venkat Madira on 17/06/2017.
+//  Copyright © 2017 Venkat Madira. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
