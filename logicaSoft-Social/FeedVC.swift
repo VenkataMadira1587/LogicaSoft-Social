b91c86fc75cb95c5917f223a60afeba08e46e81a
Venkat Madira <VenkatMadira@venkat-madira.home>
2017-06-20 20:26:18 +0100

    firebase-post result feed checks done

--- logicaSoft-Social/FeedVC.swift
+++ logicaSoft-Social/FeedVC.swift
@@ -19,6 +19,10 @@ class FeedVC: UIViewController, UITableViewDelegate,UITableViewDataSource{
         super.viewDidLoad()
         tableView.delegate=self
         tableView.dataSource = self
+        
+        DataServices.ds.REF_POSTS.observe(.value, with: { (snapshot) in
+            print(snapshot.value!)
+        })
 
         
     }
